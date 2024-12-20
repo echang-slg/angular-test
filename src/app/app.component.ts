99f55662502a6fdc31209a02505fd244103a7790
Eric Chang <ericxchang@gmail.com>
2024-12-20 17:10:01 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,   //default is true in angulalr version >= 19
   imports: [
-    CommonModule, RouterOutlet, RouterLink, RouterLinkActive
+    HeaderComponent, CommonModule, RouterOutlet, RouterLink, RouterLinkActive
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
