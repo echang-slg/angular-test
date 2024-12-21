28803f108bdf86867f603f5d97d0ac406eacb134
Eric Chang <ericxchang@gmail.com>
2024-12-21 14:20:25 -0500

    use module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,11 @@
 
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
 import dummyUserData from '@mock-data/dummy-users.json';
 import { User } from '@model';
-import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
-  imports: [
-    HeaderComponent, CommonModule, RouterOutlet, RouterLink, RouterLinkActive,
-    UserComponent,
-    TasksComponent
-],
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,30 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
+import { TasksComponent } from "./tasks/tasks.component";
 
 @NgModule({
   declarations: [
+    AppComponent
   ],
   imports: [
     CommonModule,
-    AppComponent, // Declare the root component
     BrowserModule, // Import BrowserModule for web apps
     LoggerModule.forRoot({
       level: NgxLoggerLevel.DEBUG,       // Set default log level
       serverLogLevel: NgxLoggerLevel.ERROR, // Log errors to the server
       serverLoggingUrl: '/api/logs'     // Optional: Server endpoint for logs
     }),
+    HeaderComponent,
+    UserComponent,
+    TasksComponent,
+    RouterOutlet, RouterLink, RouterLinkActive,
     RouterModule.forRoot(appRoutes, { bindToComponentInputs: true})
   ],
   providers: [], // Add services at the root level
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
