d9af9ff525a113e34b9fa32fdb8e8a6c2775aac8
Eric Chang <ericxchang@gmail.com>
2024-12-20 20:26:33 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
+import dummyUserData from './user/dummy-users.json';
+import { User } from './models';
 
 @Component({
   selector: 'app-root',
@@ -17,4 +19,5 @@ import { UserComponent } from "./user/user.component";
 })
 export class AppComponent {
   title = 'angular-test';
+  users: User[] = dummyUserData as User[];
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, input, computed, signal } from '@angular/core';
 import { User } from '../models';
 import dummyUserData from './dummy-users.json';
 
@@ -10,7 +10,32 @@ import dummyUserData from './dummy-users.json';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  /*
   users: User[] = dummyUserData as User[];
-  selectedUser = this.users[0];
+  selectedUser = signal(this.users[0]);    //use signal to track state change
 
+  imagePath = computed(() => `assets/images/${this.selectedUser().avatar}`)
+
+  // if not use signal, use this getter function to computer
+  get imageAvatar() {
+    return `assets/images/${this.selectedUser().avatar}`;
+  }
+
+  onClickUser() {
+    const randomIndex = Math.floor(Math.random() * this.users.length);
+    this.selectedUser.set(this.users[randomIndex]);
+  }
+
+  */
+
+  // @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+
+  avatar = input.required();   // using signal
+
+  imagePath = computed(() => `assets/images/${this.avatar()}`)
+
+  onClickUser() {
+
+  }
 }
